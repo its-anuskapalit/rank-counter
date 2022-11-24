3b91fe68597fe82217b09516ef1c6220bb29a180
Anuska Palit <113778308+its-anuskapalit@users.noreply.github.com>
2022-11-24 18:17:47 +0530

    Update pythonCh2.py

--- pythonCh2.py
+++ pythonCh2.py
@@ -1,17 +1,17 @@
-import calendar
-a=input("enter the first date")
-a1=a.split("/")
-start=int(a1[2])
-b=input("enter the first date")
-b1=b.split("/")
-end=int(b1[2])
-print("the range is: from",start,"to",end)
-leap=[]
-nonleap=[]
-for i in range(start,end+1):
-    if(calendar.isleap(i)==False):
-        leap.append(i)
-    else:
-        nonleap.append(i)
-print("list of leap year is:",leap)
-print("list of non leap year is:",nonleap)
+#project
+names=input("enter names:")
+name_list=names.split(",")
+marks=input("enter marks:")
+marks_list=marks.split(",")
+d=dict(zip(name_list,marks_list))
+print(d)
+marks2=input("enter the updated marks: ")
+mark2_list=marks2.split(",")
+new_marks=[]
+for i in range(0,len(marks_list)):
+    new_marks=int(marks_list[i])+int(mark2_list[i])
+max=max(new_marks)
+d2=dict(zip(str(new_marks),name_list))
+for x,y in d2.items():
+    if x==max(d2):
+        print("maximum marks is got by:",y,"and marks is:",max)
